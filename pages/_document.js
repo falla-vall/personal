8b2247c0b749b7983d2a27462f1f17a3bfad005a
--get <vall@aikara.me>
2022-04-02 11:44:30 +0700

    Get rid warning of crossOrigin attr

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,11 @@ export default function Document() {
       <Head>
         <link rel="icon" href="/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap"
           rel="stylesheet"
