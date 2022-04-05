3882486810f255fe1f4044014dd10f2c727d8f40
--get <vall@aikara.me>
2022-04-05 11:29:13 +0700

    I forgor 🩻

--- utils/api.ts
+++ utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://flla.my.id/api",
 });
 
 export const setAuthToken = (token: string) => {
