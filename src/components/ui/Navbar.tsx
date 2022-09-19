fb96af56a26ed31cf89657ba60a19744d3ac6081
Fallah Andy Prakasa <fallahandyprakasa26@gmail.com>
2022-09-19 07:32:15 +0700

    staging: Add header logo and regroup hello's

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -8,6 +8,9 @@ const Navbar: FC = () => {
   const router = useRouter();
   return (
     <nav className="sticky top-auto bottom-0 overflow-hidden w-full bg-black flex-shrink-0 flex flex-row lg:(top-0 right-0 w-auto h-screen flex-col) items-center justify-center gap-4 p-4">
+      <div className="absolute hidden lg:(block) top-0 left-0 px-4 py-12">
+        <h1 className="text-4xl font-bold">flla.</h1>
+      </div>
       {navs.map((nav, index) => {
         const isActive = router.pathname === nav.path;
         return nav.isSoon ? (
@@ -25,7 +28,9 @@ const Navbar: FC = () => {
               });
             }}
             className={`relative group md:w-32 p-2 rounded transition-all duration-300 ease ${
-              isActive ? "w-24 overflow-clip bg-white text-black" : "text-white"
+              isActive
+                ? "w-24 overflow-clip bg-white text-black"
+                : "w-9 text-white"
             }`}
           >
             <div className="flex items-center gap-2 group-focus:(sticky z-1 text-black)">
@@ -46,7 +51,7 @@ const Navbar: FC = () => {
               className={`relative group md:w-32 p-2 rounded transition-all duration-300 ease ${
                 isActive
                   ? "w-24 overflow-clip bg-white text-black"
-                  : "text-white"
+                  : "w-9 text-white"
               }`}
             >
               <div className="flex items-center gap-2 group-focus:(sticky z-1 text-black)">
