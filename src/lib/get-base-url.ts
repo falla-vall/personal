b048ad0a6e5f4b437fc4c20947e0ccf600ab498b
Fallah Andy Prakasa <me@flla.my.id>
2023-06-15 01:01:33 +0700

    fix: get url from `VERCEL_URL` directly instead of static

--- src/lib/get-base-url.ts
+++ src/lib/get-base-url.ts
@@ -1,7 +1,6 @@
 import { cache } from "react";
 
-export const getBaseUrl = cache(() =>
-  process.env.VERCEL_URL
-    ? `https://flla.vercel.app`
-    : `http://localhost:${process.env.PORT ?? 3000}`,
+export const getBaseUrl = cache(
+  () =>
+    process.env.VERCEL_URL || `http://localhost:${process.env.PORT ?? 3000}`,
 );
